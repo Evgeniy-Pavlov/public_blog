3500ac7591f1e61f21cd2e1593cd44fc8adca33c
Evgeniy-Pavlov <pavlovev@it2g.ru>
2023-05-06 18:36:13 +0300

    создана форма регистрации

--- blog/mainapp/models.py
+++ blog/mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
 # Create your models here.
+class UserBase(AbstractUser):
+    login = models.CharField(unique=True, max_length=10)
+    email = models.EmailField(unique=True)
+    last_name = models.CharField(null=True, max_length=20)
+    first_name = models.CharField(null=True, max_length=20)
+    patronymic = models.CharField(null=True, max_length=20)
+    date_create = models.DateTimeField(default=datetime.now)
+    is_company = models.BooleanField(default=False)
+    company = models.CharField(null=True, max_length=20, unique=True)
+    about_me = models.CharField(null=True, max_length=500)
+    logo = models.ImageField(null=True)
--- blog/mainapp/views.py
+++ blog/mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import UserBase
 
 # Create your views here.
+class RegisterView(CreateView):
+    model = UserBase
+    fields = '__all__'
